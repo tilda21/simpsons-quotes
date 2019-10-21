00ea15c405ea503d7cec0807d5639310336242df
tilda21 <tilda21@git.com>
2019-10-21 19:21:05 +0100

    React 3 exercise first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Quotes from "./Quotes";
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: true
+    };
+  }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  };
+  render() {
+    const work = this.state.working? "working": "break";
+    const move = this.state.working? "App-logo" : "App-logo-stop" ;
+    console.log(work);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={move} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+          <button
+          onClick={this.handleClick} className={work}>
+            {work}
+        </button>
+        </header>
+        
+       
+        <Quotes/>
+      </div>
+    );
+  }
 }
 
 export default App;
